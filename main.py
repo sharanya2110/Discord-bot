bd8b5effe7cb136c17af38b61ba6d3cd059a0ded
Sharanya Manohar <36324664+sharanya2110@users.noreply.github.com>
2022-05-13 18:48:15 +0000

    word specific response commit

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import os
 import discord
 import requests
 import json
+import random
 
 
 client =  discord.Client()
+
+sad_words = ["sad","depressed","unhappy","angry","miserable","depressing","suffocating"]
+
+starter_encouragements = ["Cheer up!","Hang in there", "You are a great person"]
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -31,11 +36,18 @@ async def on_message(message):
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
 
+  msg = message.content
+
   #type '$inspire' and the bot displays an inspirational quote 
 
   if message.content.startswith('$inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  #when bot comes across any words from the sad_words list the bot responds with a random string fromt he starter_encouragements list
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv('TOKEN'))
   
