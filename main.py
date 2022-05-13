6721813bbc7742d639fac06f43e46ccea1941d46
Sharanya Manohar <36324664+sharanya2110@users.noreply.github.com>
2022-05-13 18:07:10 +0000

    $inspire keyword commit

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
 import discord
+import requests
+import json
+
 
 client =  discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  #refer to documentation of api for loading syntax
+  json_data = json.loads(response.text)
+  # 'q' is the key for quote; 'a' is the key for author as per the api doc
+  quote = json_data[0]['q'] + "-" + json_data[0]['a']
+  return quote
 
 #register the event
 @client.event
@@ -15,9 +26,16 @@ async def on_message(message):
   #dont do anything if message is from self
   if message.author == client.user:
     return
+  #type '$hello' on discord and the bot will reply
 
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
+
+  #type '$inspire' and the bot displays an inspirational quote 
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
 
 client.run(os.getenv('TOKEN'))
   
