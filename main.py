8eeb3890eab57bad446ab31b9e59ab31321a36c1
Sharanya Manohar <36324664+sharanya2110@users.noreply.github.com>
2022-05-15 01:12:25 +0000

    create flask web server

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import json
 import random
 from replit import db
+from keep_alive import keep_alive
 
 
 client =  discord.Client()
@@ -103,6 +104,7 @@ async def on_message(message):
       db["responding"] = False
       await message.channel.send("Responding is off.")
 
+keep_alive()
 client.run(os.getenv('TOKEN'))
   
   
